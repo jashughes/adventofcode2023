6881424ca85262b7b07fb9d4968cd6af6499a7e3
jashughes <jasmine.hughes@berkeley.edu>
2023-12-04 01:01:56 -0500

    print answer

--- 03.R
+++ 03.R
@@ -14,8 +14,6 @@ get_positions <- function(v, syms) {
 check_pos <- function(start, end, y) (start - 1 <= y) & (end + 1 >= y) 
 
 # Part 1
-cd <- get_positions(instrl, setdiff(unique(instrl), c(".", as.character(0:9))))
-
 check_row <- function(sts, eds, y, s) {
   tot <- 0
   for (j in 1:length(sts)) {
@@ -26,13 +24,14 @@ check_row <- function(sts, eds, y, s) {
   tot
 }
 
-alltot <- 0
+cd <- get_positions(instrl, setdiff(unique(instrl), c(".", as.character(0:9))))
+tot1 <- 0
 for (i in 1:nrow(cd)) {
   for (x in setdiff(c(cd[i,1] - 1, cd[i,1], cd[i,1] + 1), c(0, length(instr) + 1))) {
-    alltot = alltot + check_row(starts[[x]], ends[[x]], cd[i,2], instr[[x]])
+    tot1 <- tot1 + check_row(starts[[x]], ends[[x]], cd[i,2], instr[[x]])
   }
 }
-alltot
+tot1
 
 # Part 2
 cd <- get_positions(instrl, "*")
@@ -47,16 +46,14 @@ find_adj <- function(sts, eds, y, s) {
   adj
 }
 
-alltot  <- 0
+tot2  <- 0
 for (i in 1:nrow(cd)) {
   new_adj <- c()
   for (x in setdiff(c(cd[i,1] - 1, cd[i,1], cd[i,1] + 1), c(0, length(instr) + 1))) {
     new_adj <- c(new_adj, find_adj(starts[[x]], ends[[x]], cd[i,2], instr[[x]]))
   }
-  if (length(new_adj) == 2) {
-    alltot <- alltot + prod(new_adj)
-  }
+  if (length(new_adj) == 2) tot2 <- tot2 + prod(new_adj)
 }
-alltot
 
-
+message("Part 1: ", tot1)
+message("Part 1: ", tot2)
