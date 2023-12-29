3434d015b19c437aabb137364e02ff87347f8dd6
jasmineirx <jasmine@insight-rx.com>
2023-12-28 19:57:39 -0500

    tidy

--- 25.R
+++ 25.R
@@ -1,27 +1,16 @@
 library(igraph)
-instr <- readLines("25.txt", warn = FALSE) |>
-  strsplit(": +")
+instr <- strsplit(readLines("25.txt", warn = FALSE), ":* +")
 
-l <- list()
-for (i in 1:length(instr)) {
-  s <- instr[[i]]
-  s2 <- strsplit(s[2], " +")[[1]]
-  l[[s[1]]] <- c(l[[s[1]]], s2)
-  for (sn in s2) {
-    l[[sn]] <- c(l[[sn]], s[1])
+make_graph <- function(instr) {
+  nodes <- unique(unlist(instr))
+  g <- make_undirected_graph(NULL, n = length(nodes))
+  for (i in 1:length(instr)) {
+    for (w in instr[[i]][-1]) {
+      g <- add_edges(g, c(which(nodes == instr[[i]][1]), which(nodes == w)))
+    }
   }
+  g
 }
 
-nodes <- unique(unlist(l))
-g <- make_undirected_graph(NULL)
-g <- add_vertices(g, length(nodes))
-
-for (n in nodes) {
-  nbs <- l[[n]]
-  for (nb in nbs) {
-    g <- add_edges(g, c(which(nodes == n), which(nodes == nb)))
-  }
-}
-
-cuts <- min_cut(g, value.only = FALSE)
+cuts <- min_cut(make_graph(instr), value.only = FALSE)
 message("Part 1: ", length(cuts$partition1) * length(cuts$partition2))
